4447b71ea6549b2cb97b3165b6203256e2af526a
sylvain-josserand <sylvain@intuitivo.fr>
2017-11-13 15:15:26 +0100

    First commit

---
+++ formation/core/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from decimal import Decimal
+
+
+class MoneyField(models.DecimalField):
+    def __init__(self, verbose_name=None, name=None, max_digits=None,
+                 decimal_places=None, **kwargs):
+        super(MoneyField, self).__init__(
+            verbose_name=verbose_name,
+            name=name,
+            max_digits=10,
+            decimal_places=2,
+            **kwargs,
+        )
+
+
+
+class DatedModel(models.Model):
+    class Meta:
+        abstract = True
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+
+class Transaction(DatedModel):
+    label = models.CharField(max_length=100)
+    amount = MoneyField()
+
+    
