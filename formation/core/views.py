bb87b010f42a36c4112c861fd192874339b378f9
hakim_hbl <hak.tig@gmail.com>
2017-11-14 13:45:23 +0100

    Merge branch 'master' of /Users/palmier/formation/formation with conflicts.

--- formation/core/views.py
+++ formation/core/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from core.models import Transaction
 
+# Create your views here.
 
 class TransactionListView(ListView):
     model = Transaction
